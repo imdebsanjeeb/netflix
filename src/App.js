f431cb358c2f41428a750ebca27273a0e309c6e1
Sanjeeb Deb <imdebsanjeeb@gmail.com>
2021-02-09 19:35:34 +0530

    jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import { JumbotronContainer } from './containers/jumbotron'
 
 
 export default function App() {
-  return (
-    <p>Hello</p>
-  );
+  return <JumbotronContainer />
 }
 
 
